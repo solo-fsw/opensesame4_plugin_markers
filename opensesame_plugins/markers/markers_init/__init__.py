287ee1aa0da0acc1ddca53724951a75e7f85438d
spruitim <i.m.spruit@fsw.leidenuniv.nl>
2023-10-24 16:29:16 +0200

    replaced tooltip with info

--- opensesame_plugins/markers/markers_init/__init__.py
+++ opensesame_plugins/markers/markers_init/__init__.py
@@ -10,7 +10,7 @@ controls = [
         "var": "marker_device_tag",
         "label": "Device tag",
         "name": "marker_device_tag_widget",
-        "tooltip": "Enter a tag (name) to give the marker device (do not include spaces)."
+        "info": "Enter a tag (name) to give the marker device (do not include spaces)."
     }, {
         "type": "combobox",
         "var": "marker_device",
@@ -21,43 +21,43 @@ controls = [
             "Eva"
         ],
         "name": "marker_device_widget",
-        "tooltip": "The marker device type. 'ANY' will search for any device available."
+        "info": "The marker device type. 'ANY' will search for any device available."
     }, {
         "type": "line_edit",
         "var": "marker_device_addr",
         "label": "Device address",
         "name": "marker_device_addr_widget",
-        "tooltip": "The address of the marker device should be a COM address, e.g. COM1. If unknown, leave at default 'ANY' and the address will be found automatically."
+        "info": "The address of the marker device should be a COM address, e.g. COM1. If unknown, leave at default 'ANY' and the address will be found automatically."
     }, {
         "type": "line_edit",
         "var": "marker_device_serial",
         "label": "Device serial number",
         "name": "marker_device_serial_widget",
-        "tooltip": "Enter the serial number of the marker device. If unknown, leave at default 'ANY'."
+        "info": "Enter the serial number of the marker device. If unknown, leave at default 'ANY'."
     }, {
         "type": "checkbox",
         "var": "marker_crash_on_mark_errors",
         "label": "Crash on marker errors",
         "name": "marker_crash_on_mark_errors_widget",
-        "tooltip": "When checked, the task will crash when one of the following marker errors occurs: marker too short, same marker value twice, marker could not be sent to marker device."
+        "info": "When checked, the task will crash when one of the following marker errors occurs: marker too short, same marker value twice, marker could not be sent to marker device."
     }, {
         "type": "checkbox",
         "var": "marker_dummy_mode",
         "label": "Dummy mode",
         "name": "marker_dummy_mode_widget",
-        "tooltip": "When checked, dummy mode will be used (no marker device needs to be connected, use for development)."
+        "info": "When checked, dummy mode will be used (no marker device needs to be connected, use for development)."
     }, {
         "type": "checkbox",
         "var": "marker_gen_mark_file",
         "label": "Generate marker file",
         "name": "marker_gen_mark_file_widget",
-        "tooltip": "When checked, a tsv file will be created containing a marker table, marker summary and errors."
+        "info": "When checked, a tsv file will be created containing a marker table, marker summary and errors."
     }, {
         "type": "checkbox",
         "var": "marker_flash_255",
         "label": "Flash 255",
         "name": "marker_flash_255_widget",
-        "tooltip": "When checked, two pulses with a value of 255 will be sent on initialization. Do not use with Actiview as pulses with value 255 may pause the recording."
+        "info": "When checked, two pulses with a value of 255 will be sent on initialization. Do not use with Actiview as pulses with value 255 may pause the recording."
     }
 
 ]
